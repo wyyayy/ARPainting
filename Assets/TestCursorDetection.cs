a2cb456d96d7f0c1a3b38e6ae0def8328f745520
xiemingliang <xiemingliang@sightp.com>
2018-01-30 16:40:08 +0800

    no message

---
+++ Assets/TestCursorDetection.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestCursorDetection : MonoBehaviour 
+{
+	public Texture2D Texture;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(Input.GetKeyDown(KeyCode.D))
+		{
+			var pixels = Texture.GetPixels32();
+			Debug.Log(pixels.Length);			
+			Debug.Log(Texture.width * Texture.height);
+
+			var pos = _detect(pixels);
+			Debug.Log(pos);
+		}		
+	}
+
+	Vector2 _detect(Color32[] pixels)
+	{
+		return new Vector2(0, 0);
+	}
+}
